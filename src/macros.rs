b58a5b4d54904f5cd1f9efa52748c92d6b9abf7c
James Prestwich <james@prestwi.ch>
2025-03-18 12:32:50 -0400

    refactor: trevm_try (#92)

--- src/macros.rs
+++ src/macros.rs
@@ -1,6 +1,18 @@
 /// Unwraps a Result, returning the value if successful, or returning an errored `Trevm` if not.
 #[macro_export]
+#[deprecated = "Please use `trevm_tri!` instead"]
 macro_rules! unwrap_or_trevm_err {
+    ($e:expr, $trevm:expr) => {
+        match $e {
+            Ok(val) => val,
+            Err(e) => return Err($trevm.errored(e.into())),
+        }
+    };
+}
+
+/// Unwraps a Result, returning the value if successful, or returning an errored `Trevm` if not.
+#[macro_export]
+macro_rules! trevm_try {
     ($e:expr, $trevm:expr) => {
         match $e {
             Ok(val) => val,
